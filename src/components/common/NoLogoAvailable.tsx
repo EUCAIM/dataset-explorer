5a35dba868254440b9a70230579a76212e2966d4
palollo <palollo@i3m.upv.es>
2025-05-16 19:12:13 +0200

    Few adjustments.
    Fix chaimeleon-eu/dataset-explorer#264
    Fix chaimeleon-eu/dataset-explorer#252
    Fix chaimeleon-eu/dataset-explorer#236
    Fix chaimeleon-eu/dataset-explorer#160

--- src/components/common/NoLogoAvailable.tsx
+++ src/components/common/NoLogoAvailable.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SlashCircle } from "react-bootstrap-icons";
 
 function NoLogoAvailable({}): JSX.Element {
-    return <div className="bg-secondary w-100 h-100 d-flex flex-wrap flex-row justify-content-center align-content-center text-light">
+    return <div className="w-100 h-100 d-flex flex-wrap flex-row justify-content-center align-content-center text-secondary">
             <SlashCircle className="me-1 mt-1"></SlashCircle>
             <b>No logo available</b>
         </div>
